5b4b441c7562e5819edd80d5ad834ea0d429d4fc
Steven Roper <steven.m.roper@gmail.com>
2015-02-21 10:31:13 -0700

    Finishing step 4 and adding routing to angular app.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 var app = angular.module('miniRouting', ['ngRoute']);
+
+app.config(function($routeProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: '/js/home/homeTmpl.html',
+		controller: 'homeCtrl'
+	})
+	.when('/settings', {
+		templateUrl: '/js/settings/settingsTmpl.html',
+		controller: 'settingsCtrl'
+	})
+	.when('/products/:id', {
+		templateUrl: '/js/products/productsTmpl.html',
+		controller: 'productsCtrl'
+	})
+	.otherwise({
+		templateUrl: '/js/home/homeTmpl.html',
+		controller: 'homeCtrl'
+	});
+});
