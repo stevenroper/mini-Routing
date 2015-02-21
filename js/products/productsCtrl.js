38168c75887c6c59b402d9591aa89e289d7aa5a3
Steven Roper <steven.m.roper@gmail.com>
2015-02-21 10:44:17 -0700

    Finishing Step 6 and the mini project.

--- js/products/productsCtrl.js
+++ js/products/productsCtrl.js
@@ -1,5 +1,10 @@
 var app = angular.module('miniRouting');
 
-app.controller('productsCtrl', function() {
-	
+app.controller('productsCtrl', function($scope, $routeParams, productService) {
+	if($routeParams.id === 'shoes'){
+		$scope.productData = productService.shoeData;
+	} else if($routeParams.id === 'socks') {
+		$scope.productData = productService.sockData;
+	}
+
 });
